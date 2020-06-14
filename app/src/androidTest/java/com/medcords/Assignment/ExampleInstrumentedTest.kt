1edd6319ed35c4d3c4fa6767ec14a7471c0698ee
sushilk@siliconasiaworks.com <Sk@04091986>
2020-06-15 00:51:20 +0530

    Specs: # No taskbar # Bottom navigation should have 3 tabs, name first one as Home and other two can be anything. # Horizontal scrollview should have at least 3 items. # 4 Buttons should be named as B1, B2, B3 and B4 and take up the entire place in between HSV and bottom navigation # On clicking any button should call the following network api and toast the content returned in data.rstr
    url: https://qa-doctor.medcords.com/mhc/test_handle_click
    method: post
    content-type: "application/json"
    input: {bname: "<name of the clicked button>"}
    output: {data: {rstr: "<some random string>"}}
    
    Note that API does not need any authentication.
    Assignment to be done using Kotlin and MVVM Architecture.
    
    Assignment Submission: Not to do or upload it on github, kindly share the zip file for code and apk file and keep SAIDA in CC while submitting the assignment or any queries regarding the assignment.
    Incase of any Query regarding assignment you can reply to the same mail.
    PFA!!
    
    --
    Ritika Khandelwal
    HR and Admin
    Medcords, Kota

--- app/src/androidTest/java/com/medcords/Assignment/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/medcords/Assignment/ExampleInstrumentedTest.kt
@@ -1,24 +1,5 @@
 package com.medcords.Assignment
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
-import org.junit.Test
-import org.junit.runner.RunWith
+class ExampleInstrumentedTest {}
 
-import org.junit.Assert.*
-
-/**
- * Instrumented test, which will execute on an Android device.
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
-@RunWith(AndroidJUnit4::class)
-class ExampleInstrumentedTest {
-    @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.kotlincodes.bottomnavigationview", appContext.packageName)
-    }
-}
